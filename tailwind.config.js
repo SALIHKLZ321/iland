bf354a59ff3e1f9d9e376238a8dca764f2e19220
salihklz321 <salihklz321@gmail.com>
2024-02-04 21:21:01 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,cs,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  screens:{
+    laptop: "1000px"
+  },
+  plugins: [],
+}
+
